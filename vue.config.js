b1c0c938b5665df4b56d76cc8d38d49dd15db610
volodymyrnechyporenko <volodymyrnechyporenko@gmail.com>
2021-09-20 20:15:18 +0300

    changes to css and some structure

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
             'https://pletenyikit.com.ua/order'
         ],
         defaults: {
-          lastmod:    '2021-08-21',
+          lastmod:    '2021-09-20',
           changefreq: 'daily',
           priority:   1.0,
       },
