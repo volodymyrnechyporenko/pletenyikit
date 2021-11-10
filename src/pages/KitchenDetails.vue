969451e8742f4a345f1710debc4856cf9f7f6531
volodymyrnechyporenko <volodymyrnechyporenko@gmail.com>
2021-11-10 22:56:52 +0200

    Added amount field to kitchen

--- src/pages/KitchenDetails.vue
+++ src/pages/KitchenDetails.vue
@@ -23,6 +23,8 @@
       <div class="item-text">
         <h3>{{ product.name }}</h3>
         <p>{{ product.description }}</p>
+        <p>{{ product.dimensions }}</p>
+        <p>{{ product.amount }}</p>
         <div class="price">{{ product.price }} грн</div>
       </div>
     </div>
